1140454c642ce9d92632bb2089f02464ac37fed3
akkerise <akkerise@gmail.com>
2018-04-10 18:03:53 +0700

    Doing dashboard

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -11,24 +11,24 @@ class Bootstrap
 
         // print_r($url);
         if (empty($url[0])) {
-            require 'controllers/IndexController.php';
+            require "controllers/IndexController.php";
             $controller = new IndexController();
             $controller->index();
             return false;
         }
 
         // check and require controller file
-        $file = 'controllers/' . ucfirst(strtolower(trim($url[0]))) . 'Controller.php';
+        $file = "controllers/" . ucfirst(strtolower(trim($url[0]))) . "Controller.php";
         try{
             if (!file_exists($file)) {
-                throw new Exception("File name is: ". ucfirst(strtolower(trim($url[0]))) . 'Controller.php' ." does not exist! Directiory : " . $file);
+                throw new Exception("File name is: ". ucfirst(strtolower(trim($url[0]))) . "Controller.php" ." does not exist! Directiory : " . $file);
             }
             require $file;
         }catch(Exception $e){
             echo "<pre>"; print_r($e->getMessage()); die();
         }
 
-        $nameController = ucfirst(strtolower(trim($url[0]))) . 'Controller';
+        $nameController = ucfirst(strtolower(trim($url[0]))) . "Controller";
         $controller = new $nameController();
         $controller->loadModel($url[0]);
 
@@ -54,7 +54,7 @@ class Bootstrap
 
     private static function failed($constError = null)
     {
-        require 'controllers/FailedController.php';
+        require "controllers/FailedController.php";
         $controller = new FailedController($constError);
         $controller->index($constError);
         return false;
