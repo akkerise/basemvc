fcb9fa3a17c093ceff452f59f53252337314fc04
akkerise <akkerise@gmail.com>
2018-07-19 17:35:47 +0700

    merge dev

---
+++ app/controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+class IndexController extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->view->msg = 'IndexController';
+        $this->view->render('index/index');
+    }
+
+    public function details()
+    {
+        $this->view->render('index/index');
+    }
+}
