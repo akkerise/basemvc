e8715c17e47f85216d00ca1ea3342803e0d97e8c
akkerise <akkerise@gmail.com>
2018-07-20 11:54:07 +0700

    merge branch dev

--- config/params_failed.php
+++ config/params_failed.php
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 <?php
 define('ERROR_CONTROLLER_NOT_EXIST', 'Controller file not exist!');
 define('ERROR_MODEL_NOT_EXIST', 'Model file not exist!');
 define('ERROR_METHOD_NOT_EXIST', 'Method not exist!');
+=======
+<?php
+define('ERROR_CONTROLLER_NOT_EXIST', 'Controller file not exist!');
+define('ERROR_MODEL_NOT_EXIST', 'Model file not exist!');
+define('ERROR_METHOD_NOT_EXIST', 'Method not exist!');
+>>>>>>> dev
 define('ERROR_PARAM_NOT_EXIST', 'Param not exist!');
